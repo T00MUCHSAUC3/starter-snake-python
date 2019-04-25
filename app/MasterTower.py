96848922dbf6c305ea8405e47426d35dea024d3b
Steven Fernandez <46138752+T00MUCHSAUC3@users.noreply.github.com>
2019-04-25 14:01:21 -0400

    Update MasterTower.py

--- app/MasterTower.py
+++ app/MasterTower.py
@@ -1,87 +1,159 @@
-import Astar, math, sys, random
+import astar, math, sys, random
 
-#NOTE This function will set up our grid
-def initGrid(env, OS):
-    
-    #NOTE SET UP GRID
-    grid = []
-    for col in range(0, env[1]):
-        row = []
-        for idx in range(0, env[2]):
-            row.append(1)
-        grid.append(row)
+def grid_setup(food, width, height, snakes, mySnake, mySnakeID):
 
-    #NOTE SET UP FOOD LOCATIONS
-    food_coord = []
-    for coord in env[3]:
-        x = coord.get("x")
-        y = coord.get("y")
-        food_coord.append([x, y])
+    generic_grid = []
+    #General grid setup
+    for y in range(0, height):
+        new_list = []
+        for x in range(0, width):
+            new_list.append(1)
+        generic_grid.append(new_list)
 
-    #NOTE WHERE ARE THE OTHER SNAKES?
+    food_grid = []
+    #Food locations
+    for point in food:
+        locationX = point.get("x")
+        locationY = point.get("y")
+        food_grid.append([locationX, locationY])
 
-    get_snakes(env[4], OS[0].get("id"), grid) 
-    final_grid = [grid, food_coord]
-
-    return final_grid
-
-
-def get_snakes(snakes, ourSnakeID, init_grid):
-    
-    #NOTE Get other snakes that aren't me and return them
+    #Snake locations:
     for snake in snakes:
-        body = snake.get("body")
-        this_id = snake.get("id")
-
-        if this_id != ourSnakeID:
+        body = snake.get("body").get("data")
+        snakeID = snake.get("id")
+        if snakeID != mySnakeID:
             head = body[0]
-            x = head.get("x")
-            y = head.get("y")
+            headX = head.get("x")
+            headY = head.get("y")
 
-            top = y-1
-            bottom = y-1
-            left = x-1
-            right = x+1
+            top = headY - 1
+            bottom = headY + 1
+            left = headX - 1
+            right = headX + 1
 
             if top > 0:
-                init_grid[top][x] = 0
+                generic_grid[top][headX] = 0
             if bottom < height:
-                init_grid[bottom][x] = 0
+                generic_grid[bottom][headX] = 0
             if left > 0:
-                init_grid[y][left] = 0
+                generic_grid[headY][left] = 0
             if right < width:
-                init_grid[y][right] = 0
+                generic_grid[headY][right] = 0
 
-        for coord in body:
-            x = coord.get("x")
-            y = coord.get("y")
-            init_grid[y][x] = 0
+        for point in body:
+            pointX = point.get("x")
+            pointY = point.get("y")
+            #print("new point Y = {}, new point X = {} grid x length = {}, grid y length = {}".format(pointY, pointX,len(generic_grid[0]), len(generic_grid)))
+            generic_grid[pointY][pointX] = 0
 
-def get_food_dictionary(ourSnakeX, ourSnakeY, food):
+    grid_options = []
 
-    #NOTE Map the coordinates of food to their respective euclidean distance
-    closest_food = {}
-    for coord in food:
-        this_distance = get_distance([ourSnakeX, ourSnakeY], coord)
-        closest_food[tuple(coord)] = this_distance
+    grid_options.append(generic_grid)
+    grid_options.append(food_grid)
 
-    print(closest_food)
-    return "left"
+    '''print('')
+    for y in range(0, width):
+        print('')
+        for x in range(0, height):
+            if generic_grid[y][x] == 0 :
+                print('X', end='')
+            else:
+                print('0', end='')'''
 
-def get_distance(point_x, point_y):
+    return grid_options
 
-    """
-    This method will get the distance between two points
-    """
+def get_crows_dist(me, you):
+    (x1, y1) = me
+    (x2, y2) = you
+    return abs(math.hypot(x2 - x1, y2 - y1))
 
-    return (abs(math.hypot(point_x[0] - point_y[0], point_x[1] - point_y[1])))
+def get_my_snake_coordinates(snakes, your_id):
+    for snake in snakes:
+        if snake.get('id') == your_id:
+            return snake.get('coords')
 
-def closestFood():
+#NOTE returns a position tuple of closest food pellet
+def get_closest_food(food_list, head_x, head_y):
+    current_minimum = 10000
+    for position in food_list:
+        pellet_distance = get_crows_dist((head_x, head_y),position)
+        if pellet_distance < current_minimum:
+            current_minimum = pellet_distance
+            target_position = position
+    return tuple(target_position)
 
-    """
-    This method will get the closest food to our snake
-    """
+def get_neighbors(node, lines, height, width):
+    (x, y) = node #changed from x, y
+    return[(nx, ny) for nx, ny in[(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)] if 0 <= nx < width and 0 <= ny < height and lines[ny][nx] == 1]
 
-    infinity = float('inf')
-    
-    return 0
+def get_move_letter(start, end):
+    currX = start[0]
+    currY = start[1]
+    nextX = end[0]
+    nextY = end[1]
+    deltaX = nextX - currX
+    deltaY = nextY - currY
+    if deltaX > 0:
+        return 'right'
+    elif deltaY > 0:
+        return 'down'
+    elif deltaX < 0:
+        return 'left'
+    elif deltaY < 0:
+        return 'up'
+
+
+def move_to_food(a_star_object, food_list, head_x, head_y):
+    current_minimum = float('inf')
+    current_path = None
+    for food in food_list:
+        path = a_star_object.astar((head_x, head_y), tuple(food))
+        if path:
+            path = list(path)
+            if len(path) < current_minimum:
+                current_minimum = len(path)
+                current_path = path
+    if current_path:
+        return get_move_letter((head_x, head_y), list(current_path)[1])
+    return None
+
+def chase_tail(a_star_object, grid_options, mySnake, head_x, head_y, isGonnaGrow, height, width):
+    myTail = (mySnake[-1].get("x"), mySnake[-1].get("y"))
+    grid_options[0][myTail[1]][myTail[0]] = 1
+    path = a_star_object.astar((head_x, head_y), myTail)
+    grid_options[0][myTail[1]][myTail[0]] = 0
+    if path:
+        if not isGonnaGrow:
+            return get_move_letter((head_x, head_y), list(path)[1])
+        else:
+            neighbours = get_neighbors(myTail, grid_options[0], height, width)
+            for neighbour in neighbours:
+                path = a_star_object.astar((head_x, head_y), neighbour)
+                if path:
+                    return get_move_letter((head_x, head_y), list(path)[1])
+
+
+    return None
+
+def get_move(grid_options, target, head_x, head_y, height, width, mySnake, myHealth):
+    a_star_object = astar.AStarAlgorithm(grid_options[0], width, height)
+
+    myLength = len(mySnake)
+    move = move_to_food(a_star_object, grid_options[1], head_x, head_y)
+
+    #NOTE FIND TAIL MODE
+    if myLength > 3 and myHealth > 75 or move == None: #85
+        gonnaGrow = False
+        if myHealth == 100:
+            gonnaGrow = True
+        move = chase_tail(a_star_object, grid_options, mySnake, head_x, head_y, gonnaGrow, height, width)
+
+
+    if move:
+        return move
+    else:
+        neighbours = get_neighbors((head_x, head_y), grid_options[0], height, width)
+        if neighbours:
+            return get_move_letter((head_x, head_y), neighbours[0])
+        else:
+            return 'left'
