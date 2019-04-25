708d1690407a07930c3df39c72f770c4ddc81f04
S3 <sfernandez0201@gmail.com>
2019-04-24 21:08:57 -0400

    .

--- app/MasterTower.py
+++ app/MasterTower.py
@@ -19,11 +19,45 @@ def initGrid(env, OS):
         print("FOOD: ", x, y)
         food_coord.append([x, y])
 
+    #NOTE WHERE ARE THE OTHER SNAKES?
+
+    get_snakes(env[4], OS[0].get("id"), grid) 
     final_grid = [grid, food_coord]
 
     return final_grid
 
-    #NOTE Figure out how to handle snakes that aren't me...
+
+def get_snakes(snakes, ourSnakeID, init_grid):
+    
+    #NOTE Get other snakes that aren't me and return them
+    for snake in snakes:
+        body = snake.get("body")
+        this_id = snake.get("id")
+
+        if this_id != ourSnakeID:
+            head = body[0]
+            x = head.get("x")
+            y = head.get("y")
+
+            top = y-1
+            bottom = y-1
+            left = x-1
+            right = x+1
+
+            if top > 0:
+                init_grid[top][x] = 0
+            if bottom < height:
+                init_grid[bottom][x] = 0
+            if left > 0:
+                init_grid[y][left] = 0
+            if right < width:
+                init_grid[y][right] = 0
+
+        for coord in body:
+            x = coord.get("x")
+            y = coord.get("y")
+            init_grid[y][x] = 0
+
 
 def get_distance(point_x, point_y):
 
@@ -40,7 +74,5 @@ def closestFood():
     """
 
     infinity = float('inf')
-    for food in food:
-        #do something
     
     return 0
